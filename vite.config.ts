c6721f8375dc1daddc50dfb814b8411aedc0ed42
Artem Nakonechnyi <artem301986@icloud.com>
2025-01-08 18:00:00 +0200

    add yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/gym-typescript-learning/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
